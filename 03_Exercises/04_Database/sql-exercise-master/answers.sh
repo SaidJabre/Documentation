c405093fcae71aed7e5408b4ca4177c6903eab78
said jabre <saidjabre@gmail.com>
2022-01-28 22:21:46 +0200

    Basic Queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,17 @@
+0. select * from students
+1. Select name from students
+2. select * from students 
+    where age > 30
+3. select name from students
+	where gender="F" and age=30
+4. select name from students
+	where gender="F" and age=30
+5. insert into students (name,age,gender,points) 
+	values("Said",47,"M",300)
+6. update students
+	set points=points+100
+	where name="Basma"
+7. update students
+	set points=points-50
+	where name="Alex"
+8. 
